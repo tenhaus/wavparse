c55288e35ecd15a83fab545de6e39530754988b6
Chris Hayen <chris.hayen@performancehorizon.com>
2016-11-18 15:09:48 -0500

    Parsing ChunkSize

--- parse.go
+++ parse.go
@@ -1,7 +1,17 @@
 package wavparse
 
+import "encoding/binary"
+
 // Header returns the first 4 bytes of the file
 // which should equal RIFF
 func Header(wav []byte) []byte {
 	return wav[0:4]
+}
+
+// ChunkSize returns the chunksize
+// 4 + (8 + SubChunk1Size) + (8 + SubChunk2Size)
+// or 36, which is the meta size + SubChunk2Size
+func ChunkSize(wav []byte) uint32 {
+	size := binary.LittleEndian.Uint32(wav[4:8])
+	return size
 }
--- parse_test.go
+++ parse_test.go
@@ -7,6 +7,7 @@ import (
 
 var wav []byte
 
+// http://soundfile.sapp.org/doc/WaveFormat/
 func init() {
 	wav, _ = ioutil.ReadFile("test/322968.wav")
 }
@@ -14,6 +15,15 @@ func init() {
 func TestHeader(t *testing.T) {
 	header := Header(wav)
 	if string(header) != "RIFF" {
+		t.Fail()
+	}
+}
+
+// 226240 minus 8 bytes for ChunkID and ChunkSize which
+// are not included in the total
+func TestChunkSize(t *testing.T) {
+	size := ChunkSize(wav)
+	if size != 226232 {
 		t.Fail()
 	}
 }
